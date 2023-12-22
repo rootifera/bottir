b66b4796f6fef46e0fa300990ee3b5940648de8e
Omur Ozbahceliler <omur@rootifera.org>
2023-12-22 20:31:11 +0000

    packagecheck.py updated

--- packagecheck.py
+++ packagecheck.py
@@ -9,17 +9,21 @@ def is_required_package_installed(package_name, distro):
             if distro == 'debian' or distro == 'ubuntu':
                 result = subprocess.run(['dpkg', '--get-selections'], stdout=subprocess.PIPE, text=True, check=True)
                 installed_packages = result.stdout.split('\n')
+                print(installed_packages[0])
         # Run dnf command to list installed packages
-        if os.path.isfile('/usr/bin/dnf'):
+        elif os.path.isfile('/usr/bin/dnf'):
             if distro == 'redhat' or distro == 'centos' or distro == 'fedora':
                 result = subprocess.run(['dnf', 'list', 'installed'], stdout=subprocess.PIPE, text=True, check=True)
                 installed_packages = result.stdout.split('\n')
         else:
+            print('else')
             return False
 
         # Check if the specified package is in the list
         for package in installed_packages:
+            print("pack check")
             if package.startswith(package_name):
+                print('package found')
                 return True
 
         # Package not found
